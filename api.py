2d21b26ef2cbc6f8e510a3e66981ebed0e426f16
meows4h <dustyisntsorusty@gmail.com>
2025-12-06 06:37:12 -0800

    trying things

--- api.py
+++ api.py
@@ -5,16 +5,33 @@ from fflogsapi import FFLogsClient, GQLEnum
 client = FFLogsClient(CLIENT_ID, CLIENT_SECRET)
 
 
-def get_player_casts(rid, number, player):
+def pull_player(info, player, idx):
+    ''''''
+    for entry in info[idx]:
+        if player in entry.values():
+            return entry
+
+
+def get_player_data(rid, number, player):
     ''''''
     report = client.get_report(rid)
     fight = report.fight(number)
-    casts = fight.table({"dataType": GQLEnum("Casts")})
-    for entity in casts["entries"]:
-        if player in entity.values():
-            return entity
+    damage = fight.events({"dataType": GQLEnum("DamageDone")})
+    casts = fight.events({"dataType": GQLEnum("Casts")})
+    summary = fight.table({"dataType": GQLEnum("Summary")})
+    events = fight.events({"dataType": GQLEnum("All")})
+    
+    print(casts)
+    print(damage)
+    #print(events)
 
-    return None
+    data = {}
+
+    data["damage"] = pull_player(damage, player, "entries")
+    data["casts"] = pull_player(casts, player, "entries")
+    data["time"] = summary["totalTime"] 
+
+    return data
 
 
 def compare_casts(cast_name, x, y):
@@ -25,9 +42,9 @@ req_logs = [["kNn2rcQKLZCTgHdh", 46, "Meows For'heals"], ["hdzNJKTx8Grv9tAY", 3,
 cast_data = []
 
 for req in req_logs:
-    cast_data.append(get_player_casts(req[0], req[1], req[2]))
+    cast_data.append(get_player_data(req[0], req[1], req[2]))
 
-print(cast_data[1]["abilities"])
+print(cast_data[1])
 
 client.close()
 client.save_cache()
